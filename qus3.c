d9a5511aff12cb1055505209647be9bbe5162616
janani20 <janushalu20@gmail.com>
2016-07-10 12:17:37 +0530

    Create qus3.c

---
+++ qus3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("\n Enter any character:");
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+printf("\n charater %c you enter is vowel",a);
+esle
+printf("charater %c you enter is consonant",a);
+}
