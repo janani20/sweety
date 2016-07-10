eb0cd569198af2a04f24a5fb84f94bf8566e30de
janani20 <janushalu20@gmail.com>
2016-07-10 12:05:58 +0530

    Create qus1.c

---
+++ qus1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\n Enter the number :");
+scanf("%d",&n);
+if(n==0)
+printf("Given number is zero);
+else if(n>=0)
+printf("Given number is positive);
+else
+printf("Given number is negative");
+}
