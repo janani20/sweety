d90ceec024fe1be95bcf677eca3aa837e4a3e468
janani20 <janushalu20@gmail.com>
2016-07-10 12:25:20 +0530

    Create qus5.c

---
+++ qus5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a=55,b=10,c=78;
+if(a>b&&a>c)
+printf("a = %d is greater ",a);
+else if(b>c&&b>a)
+printf("b = %d is greater ",b);
+else
+printf("c = %d is greater ",c);
+}
