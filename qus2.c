a9d6bfd87e0eebcaaaf9daa4a6e181ad63980f68
janani20 <janushalu20@gmail.com>
2016-07-11 10:42:32 +0530

    Create qus2.c

---
+++ qus2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\n Enter the number :");
+scanf("%d",&n);
+if(n%2==0)
+{ 
+printf("\n Given num is even");
+}
+else
+printf("\n Given num is odd");
+getch();
+}
