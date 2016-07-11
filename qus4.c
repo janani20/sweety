1a3ff64a75df99b4654c2b4d5f54e241d2245e26
janani20 <janushalu20@gmail.com>
2016-07-11 10:45:54 +0530

    Create qus4.c

---
+++ qus4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\n Enter the number :");
+scanf("%d",&n);
+if(n%4==0)
+{ 
+printf("\n Given year is leap year");
+}
+else
+printf("\n Given year is nonleap year");
+getch();
+}
